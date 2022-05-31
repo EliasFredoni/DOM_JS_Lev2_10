ce01c5eeaf592a5bf940531d85fddcf5f207eb66
EliasFredoni <fredoni.elias@gmail.com>
2022-05-31 15:10:11 +0200

    fonts

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+let change = document.getElementById("element");
+
+let myFunction1 = () => {
+    element.style.borderStyle = "solid";
+    element.style.borderWidth = "5px";
+    element.style.borderColor = "black"
+    element.style.color = "green";
+}
+
+let myFunction2 = () => {
+    element.style.borderStyle = "solid"
+    element.style.borderWidth = "10px";
+
+}
+
+let myFunction3 = () => {
+    element.style.borderStyle = "solid";
+    element.style.borderWidth = "15px";
+    element.style.borderColor = "green";
+}
